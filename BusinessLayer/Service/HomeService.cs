8f689e48c88a9c7bd69923e336437d3127ef153d
PhongNguyen <phongnv0910@gmail.com>
2024-02-29 23:27:03 +0700

    fix reference

--- BusinessLayer/Service/HomeService.cs
+++ BusinessLayer/Service/HomeService.cs
@@ -1,17 +1,19 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Data_Access_Layer.Models
-//namespace Business_Layer.Service
-//{
-//    public class HomeService
-//    {
-//        public List<Product> GetListProduct()
-//        {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
 
-//        }
 
-//    }
-//}
+namespace Business_Layer.Service
+{
+    public class HomeService
+    {
+        public List<Product> GetListProduct()
+        {
+            return new List<Product>();
+        }
+
+    }
+}
