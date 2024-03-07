934cd2a96d52629697a8879b52ebef9e10cf29e2
PhongNguyen <phongnv0910@gmail.com>
2024-03-07 22:42:36 +0700

    add blog

--- DataAccessLayer/Repository/BlogRepository.cs
+++ DataAccessLayer/Repository/BlogRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Data_Access_Layer.Repository;
+using DataAccessLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repository
 {
-    public class BlogRepository
+   
+    public class BlogRepository : Repository<Blog>
     {
+        public BlogRepository(SWDContext context) : base(context)
+        {
+        }
+
     }
 }
