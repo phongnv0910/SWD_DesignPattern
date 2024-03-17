bb4dd2261ddbdacf797be54775963da10d894d51
PhongNguyen <phongnv0910@gmail.com>
2024-03-17 19:35:03 +0700

    fix

---
+++ PresentationLayer/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Login(string account, string )
+        {
+            return View();
+        }
+    }
+}
